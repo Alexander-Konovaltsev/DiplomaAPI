08ccdf32501fe3cf2a049247042a7ee1bbe9660b
Alexander-Konovaltsev <aleksandar.konovaltseff@yandex.ru>
2026-04-07 21:16:02 +0300

    Добавляет создание тестового пользователя при инициализации

--- api/db/db_initializer.py
+++ api/db/db_initializer.py
@@ -1,11 +1,15 @@
 from db.session import SessionLocal
 from models.role import Role
+from models.user import User
+from crud.user import get_user_by_email
+from services.security_service import SecurityService
 
 class DBInitializer:
     def __init__(self):
         self.db = SessionLocal()
         try:
             self.init_roles()
+            self.init_test_user()
         finally:
             self.db.close()
 
@@ -22,4 +26,23 @@ class DBInitializer:
             if not exists:
                 self.db.add(Role(**role))
 
+        self.db.commit()
+
+    def init_test_user(self):
+        email = "test@test.ru"
+
+        if get_user_by_email(self.db, email):
+            return
+        
+        test_user = User(
+            last_name="Тестовый",
+            first_name="Тест",
+            patronymic="Тестович",
+            email=email,
+            password=SecurityService.hash_password("1234"),
+            role_id=1,
+            workplace="Тестовый университет"
+        )
+
+        self.db.add(test_user)
         self.db.commit()
